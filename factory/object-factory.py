2192e7a39a7f2e6294ad23da8da97c98b78ce2d5
min-0 <dhe08133@naver.com>
2021-01-11 21:51:47 +0900

    faker test

--- factory/object-factory.py
+++ factory/object-factory.py
@@ -1,11 +1,28 @@
 from faker import Faker
 
+class Ipman :
+    
+    def __init__ (self, name, address, ip):
+        self.name = name
+        self.address = address
+        self.ip = ip
 
-
-
+    def show_data(self):
+        print('name :', self.name)
+        print('address :', self.address)
+        print('ip :', self.ip)
 
 if __name__ == "__main__":
     f = Faker('ko_KR')
 
-    for i in range(10):
-        print(f.name())
+    #가상 환경 쓸 경우, 버퍼에 가상환경 주소가 남아 있는 것을 해결하기 위해
+    input()
+
+    number = int(input('몇개 생성하실 ? > '))
+    Ipmans = []
+
+    for i in range(int(number)):
+        Ipmans.append(Ipman(f.name(), f.address(), f.ipv4_private()))
+
+    for j in Ipmans:
+        j.show_data()
